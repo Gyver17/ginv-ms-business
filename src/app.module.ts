fe47933153300812f41ba7888beda1fb930fe612
Jose Angel Cortez Mata <acortez@cmlexports.com>
2022-08-15 23:46:43 -0400

    initials setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    CategoriesModule,
+  ],
 })
 export class AppModule {}
---
+++ src/database/database.providers.ts
@@ -0,0 +1,29 @@
+import { Sequelize } from 'sequelize-typescript';
+import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../common/constants';
+import databaseVariables from './database.config';
+
+export const databaseProviders = [
+  {
+    provide: SEQUELIZE,
+    useFactory: async () => {
+      const databaseConfig = databaseVariables();
+      let config;
+      switch (process.env.NODE_ENV as any) {
+        case DEVELOPMENT:
+          config = databaseConfig.development;
+          break;
+        case TEST:
+          config = databaseConfig.test;
+          break;
+        case PRODUCTION:
+          config = databaseConfig.production;
+          break;
+        default:
+          config = databaseConfig.development;
+      }
+      const sequelize = new Sequelize(config);
+      //   sequelize.addModels([Post]);
+      return sequelize;
+    },
+  },
+];
